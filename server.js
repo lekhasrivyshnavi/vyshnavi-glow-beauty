ef51a9acbfc67eb4005237ff8d089e70f36b30ae
vyshnavii19 <228w1a04a9@vrsec.ac.in>
2026-05-23 17:00:46 +0530

    fixed mysql

--- server.js
+++ server.js
@@ -20,9 +20,6 @@ const db = mysql.createConnection({
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
     port: process.env.MYSQLPORT,
-    ssl: {
-        rejectUnauthorized: false
-    }
 });
 
 db.connect((err) => {
