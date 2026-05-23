72f3d523511d18eb1f8d8ba7e33507fba2bf1976
vyshnavii19 <228w1a04a9@vrsec.ac.in>
2026-05-23 16:04:06 +0530

    render port setup

--- server.js
+++ server.js
@@ -944,8 +944,10 @@ app.post("/create-admin",(req,res)=>{
 
 // SERVER
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
-    console.log("Server running on port 3000");
+app.listen(PORT, () => {
+
+    console.log(`Server running on port ${PORT}`);
 
 });
