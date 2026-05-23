d3e7247752bbfbe7d450730532c3b93880e04999
vyshnavii19 <228w1a04a9@vrsec.ac.in>
2026-05-23 19:17:33 +0530

    fixed mysql connection

--- server.js
+++ server.js
@@ -14,7 +14,13 @@ app.use(express.json());
 
 // MYSQL CONNECTION
 
-const db = mysql.createConnection(process.env.DATABASE_URL);
+const db = mysql.createConnection({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
+});
 
 db.connect((err) => {
     if (err) {
